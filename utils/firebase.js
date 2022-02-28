179141e07321d7be267fad482cf395d4ac4df8a1
Snuffy <rodrigo.sgarabotto.scola@gmail.com>
2022-02-28 19:18:24 -0300

    env variables

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,15 +1,14 @@
 import { initializeApp } from "firebase/app"
 import { getStorage, ref, uploadBytes, upload } from "firebase/storage"
 
-const firebaseConfig = {
-	apiKey: "AIzaSyDhWuUUIMUczvhJEE8qiKyjpQVkruQ8ryQ",
-	authDomain: "store-dfd55.firebaseapp.com",
-	projectId: "store-dfd55",
-	storageBucket: "store-dfd55.appspot.com",
-	messagingSenderId: "516320116991",
-	appId: "1:516320116991:web:9979294f18e3ce49430cca",
-}
-const app = initializeApp(firebaseConfig)
+const app = initializeApp({
+	apiKey: process.env.FIREBASE_API_KEY,
+	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.FIREBASE_PROJECT_ID,
+	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.FIREBASE_APP_ID,
+})
 const storage = getStorage(app)
 
 module.exports = storage
